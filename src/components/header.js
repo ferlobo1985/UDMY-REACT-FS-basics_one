10a6ee9594cd4328a0da6572398c99c8cae36e38
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-04-29 01:39:08 -0300

    8-Events

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,25 @@
 import React,{ Component } from "react";
 
 
+
 class Header extends Component {
+
+    
+    inputChange = (event) => {
+        console.log(event)
+    }
+
     render(){
         return (
             <>
                 <header>
-                    <div className="logo">Logo</div>
-                    <input/>
+                    <div 
+                        className="logo"
+                        onClick={()=> console.log('I was clicked')}
+                    >Logo</div>
+                    <input
+                        onChange={(e)=> this.inputChange(e)}
+                    />
                 </header>
             </>
         )
