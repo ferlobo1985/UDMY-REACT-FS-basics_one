c686dcb2beaba380d49d2f8ca85d92aa985da1c9
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-04-29 18:06:45 -0300

    17-Component Lifecycle 2

--- src/components/lifecycles.js
+++ src/components/lifecycles.js
@@ -4,15 +4,43 @@ class Life  extends Component {
     constructor(props){
         super(props);
         console.log('1-constructor')
-
         this.state = {name:'Francis'}
     }
 
 
-    static getDerivedStateFromProps(){
+    static getDerivedStateFromProps(props,state){
         console.log('2-getDerivedStateFromProps');
+        if(state.name === 'Ron'){
+            return {
+                name:'Milhouse'
+            }
+        }
         return null;
     }
+
+    shouldComponentUpdate(nextProps,nextState){
+        console.log('x-shouldComponentUpdate');
+        if(nextState.name === 'Steve'){
+            return false
+        }
+        return true;
+    }
+
+
+    getSnapshotBeforeUpdate(prevProps,prevState){
+        console.log('x-getSnapshotBeforeUpdate')
+        let age = 20;
+        return age;
+    }
+
+
+    componentDidUpdate(prevProps,prevState, snapshot){
+        console.log('x-componentDidUpdate');
+        // console.log(prevState);
+        // console.log(this.state)
+        console.log(snapshot)
+    }
+
 
     componentDidMount(){
         console.log('4-componentDidMount');
@@ -26,7 +54,11 @@ class Life  extends Component {
         console.log('3-render');
         return(
             <>
-                Life
+                <hr/>
+                <div>{this.state.name}</div>
+                <button  onClick={()=> this.setState({name:'Ron'})}>
+                    Change name
+                </button>
             </>
         )
     }
